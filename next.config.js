eca9d9ed2622426da7730534ca4b4afed66c95d6
Quarkisinproton <quarkisinproton@gmail.com>
2025-09-22 17:25:20 +0530

    For PWA support

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// Add this to enable PWA with next-pwa
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+});
+
+module.exports = withPWA({
+  // ...your existing Next.js config
+});
---
+++ public/sw.js
@@ -0,0 +1,6 @@
+// Placeholder service worker for next-pwa
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
